8a4510e3e7468b5fe6c0ab655ec1a3c3def6d8c7
skreutz3 <skreutz3@smail.uni-koeln.de>
2022-12-12 02:57:45 +0100

    solved Homework 9

--- HW9/src/test/TestCalculator.java
+++ HW9/src/test/TestCalculator.java
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import main.Calculator;
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Assert;
+
+import main.Calculator;
 
 class TestCalculator {
 
@@ -14,25 +18,45 @@ class TestCalculator {
 	@Test
 	public void Should_add_two_numbers_and_return_result() {
 		// Implement
+		int expectedResult = 2 + 3;
+		int actualResult = calculator.add(2, 3);
+
+		assertEquals(expectedResult, actualResult);
 	}
-	
+
 	@Test
 	public void Should_substract_two_numbers_and_return_result() {
 		// Implement
+		int expectedResult = 5 - 2;
+		int actualResult = calculator.sub(5, 2);
+
+		assertEquals(expectedResult, actualResult);
+
 	}
-	
+
 	@Test
 	public void Should_multiply_two_numbers_and_return_result() {
 		// Implement
+		int expectedResult = 5 * 2;
+		int actualResult = calculator.multiply(5, 2);
+
+		assertEquals(expectedResult, actualResult);
 	}
-	
+
 	@Test
 	public void Should_divide_two_numbers_and_return_result() {
 		// Implement
+		
+		float expectedResult = 6/ 2;
+		float actualResult = calculator.divide(6, 2);
+
+		assertEquals(expectedResult, actualResult);
 	}
-	
+
 	@Test
 	public void Should_throw_an_arithmetic_exception_if_denominator_is_zero() {
 		// Implement
+		float actualResult = calculator.divide(0, 2);
+		Assert.assertThrows(ArithmeticException.class, null);
 	}
 }
--- HW9/src/test/TestCalendar.java
+++ HW9/src/test/TestCalendar.java
@@ -2,26 +2,67 @@ package test;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import java.util.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import main.Calendar;
 
 class TestCalendar {
 
+	Calendar calendar;
+	GregorianCalendar cal = (GregorianCalendar) GregorianCalendar.getInstance();
+
 	@ParameterizedTest
-	@ValueSource()
+	@ValueSource(ints = { 1920, 1916, 1996, 1904, 1960 })
 	public void Should_return_true(int year) {
 		// Implement
+
+		calendar = new Calendar(year);
+		boolean expectedResult = cal.isLeapYear(year);
+		boolean actualResult = calendar.isLeapYear();
+		assertEquals(expectedResult, actualResult);
+		
 	}
 
 	@ParameterizedTest
-	@ValueSource()
+	@ValueSource(ints = { 1825, 1919, 1955, 2002, 2021 })
 	public void Should_return_false(int year) {
 		// Implement
+		calendar = new Calendar(year);
+		boolean expectedResult = cal.isLeapYear(year);
+		boolean actualResult = calendar.isLeapYear();
+		
+		assertFalse(expectedResult == actualResult);
+
 	}
 
 	@ParameterizedTest
-	@ValueSource()
+	@ValueSource(ints = {1904, 1912, 1908, 1996, 1992})
 	public void Should_return_if_year_is_leap(int year) {
 		// Implement
+		calendar = new Calendar(year);
+		boolean expectedResult = cal.isLeapYear(year);
+		boolean actualResult = calendar.isLeapYear();
+		
+		if(actualResult == expectedResult) {
+			assertTrue(expectedResult);
+		} else {
+			assertFalse(expectedResult);
+		}
+		
 	}
 
 	// Create a new method for boundary testing
+	
+	@ParameterizedTest
+	@ValueSource(ints = {1904, 1912, 1908, 1964, 1988})
+	public void is_range_acceptable(int year) {
+		calendar = new Calendar(year);
+		if(year >= 1900 && year <2000) {
+			assertTrue(calendar.isLeapYear());
+		} else {
+			assertFalse(calendar.isLeapYear());
+		}
+	}
+	
 }
